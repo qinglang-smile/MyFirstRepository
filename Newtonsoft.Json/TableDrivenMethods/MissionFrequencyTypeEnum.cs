f0e0483d575d418470e8f4611d800c3466bea84f
qiuqing <18476686118@139.com>
2019-08-07 13:47:46 +0800

    temp

---
+++ Newtonsoft.Json/TableDrivenMethods/MissionFrequencyTypeEnum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TableDrivenMethods
+{
+    /// <summary>
+    /// 针对循环时间类型: 1.每天 2.每周 3.每月 4.每年
+    /// </summary>
+    public enum MissionFrequencyTypeEnum
+    {
+        /// <summary>
+        /// 每天
+        /// </summary>
+        Day = 1,
+
+        /// <summary>
+        /// 每周
+        /// </summary>
+        Week = 2,
+
+        /// <summary>
+        /// 每月
+        /// </summary>
+        Month = 3,
+
+        /// <summary>
+        /// 每年
+        /// </summary>
+        Year = 4,
+    }
+}
---
+++ Newtonsoft.Json/TemplateMethod/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateMethod
+{
+    public class Student : Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public override void Role()
+        {
+            Console.WriteLine("学生");
+        }
+
+        public override void EatBreakfast()
+        {
+            Console.WriteLine("吃包子");
+        }
+
+        public override void Transportation()
+        {
+            Console.WriteLine("坐校车");
+        }
+
+        public void Deconstruct(out int id, out string name)
+        {
+            id = Id;
+            name = Name;
+        }
+
+        public Student()
+        {
+
+        }
+        public Student(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
